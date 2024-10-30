33f76c8a0a86fa6b6f32847503ab8c6d3fee2e9e
semir-t <semir.tursunovic@microbitdesign.com>
2024-10-30 17:14:23 +0100

    Fix to have capability to break from regular loop

--- libraries/Algobrix/algoC.h
+++ libraries/Algobrix/algoC.h
@@ -28,6 +28,8 @@
 #endif
 
 #define repeat(system,condition)				system.cthread.sequance++;while((g_ALGOBOT_INFO.state != ALGOBOT_STATE_IDLE) && (g_ALGOBOT_INFO.state != ALGOBOT_STATE_HALT) && (condition))
+#define isStopPressed(system)                   system.cthread.sequance++;if((g_ALGOBOT_INFO.state == ALGOBOT_STATE_IDLE) || (g_ALGOBOT_INFO.state == ALGOBOT_STATE_HALT)){break;}
+
 
 
 /* Exported macros ********************************************************* */
